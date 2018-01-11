5233ecf96dd2e6d00cdddf0bbc787ffda6258a17
MrPiszczel <panpiszczel@gmail.com>
2018-01-11 14:10:50 +0100

    zadanie 1.2

---
+++ Zadanie1.2/main.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int obliczP(int p, int x){
+    int i, wynik=1;
+
+     for(i = 0 ; i < p ; i++){
+        wynik*=x;
+
+     }
+     return wynik;
+}
+
+int obliczS(int p){
+  int i, wynik=1;
+  for(i = 1 ; i < p + 1 ; i++){
+
+    wynik*=i;
+
+  }
+  return wynik;
+}
+
+double obliczCosX(int potega, int x){
+
+   double cosX = 1.0;
+   int p = 2, wynikPotegowania, wynikSilni, liczbaPowtorzen=0;
+
+   //wynikPotegowania = obliczP(p, x);
+   //printf("\n%d", wynikPotegowania);
+
+  // wynikSilni = obliczS(p);
+  // printf("\n%d", wynikSilni);
+
+   while(liczbaPowtorzen < potega){
+   if(liczbaPowtorzen % 2 == 0){
+        printf("\n pierwszy los rowny zero");
+
+        wynikPotegowania = obliczP(p, x);
+         wynikSilni = obliczS(p);
+
+         cosX -=(double) wynikPotegowania/wynikSilni;
+        liczbaPowtorzen++;
+        p+=2;
+
+   }else if(liczbaPowtorzen % 2 != 0){
+        printf("\n drugi los nie rowny 0");
+
+        wynikPotegowania = obliczP(p, x);
+         wynikSilni = obliczS(p);
+
+        cosX +=(double) wynikPotegowania/wynikSilni;
+       liczbaPowtorzen++;
+        p+=2;
+
+   }
+
+   }
+
+  return cosX;
+}
+
+int main()
+{
+
+    int potega, x;
+    double wynik;
+
+    printf("Podaj  max potege - to jest n z zadania, widzialem podobne zadanie w ktorym to n ma byc 10, a ja zrobilem ze mozna podac jaka sie chce liczbe: ");
+    scanf("%d", &potega);
+
+    printf("Podaj x");
+    scanf("%d", &x);
+
+    wynik=(double)obliczCosX(potega, x);
+    printf("\n%f", wynik);
+
+    return 0;
+}
